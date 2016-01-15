d09ce4f5f3c6962ca9f182bb6bbe5c83b9f6a346
Mike Frazier <michael.s.frazier@gmail.com>
2016-01-15 14:11:38 -0600

    Completed test cases for the linkedList program

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,35 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var nodeToAdd = Node(value);
+    if(list.head === null) {
+      list.head = nodeToAdd;
+      list.tail = nodeToAdd;
+    }
+    list.tail.next = nodeToAdd;
+    list.tail = nodeToAdd;
   };
 
   list.removeHead = function(){
+    var hold = list.head;
+    list.head = hold.next;
+    return hold.value;
   };
 
-  list.contains = function(target){
+  list.contains = function(target){ //target is the value property of the nodes
+    //iterate the list
+    var result = false;
+    var currentNode = list.head;
+    while(currentNode.next){
+      if(currentNode.value === target) {
+        result = true;
+      }
+      currentNode = currentNode.next;
+    }
+    if(currentNode.value === target) {
+      result = true;
+    }
+    return result;
   };
 
   return list;
@@ -26,4 +49,7 @@ var Node = function(value){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ .addToTail = O(1)
+ .removeHead = O(1)
+ .contains = O(n)
  */
