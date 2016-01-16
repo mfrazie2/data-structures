e3172cf68acd793f3309582c5cd8ead511a8789f
Mike Frazier <michael.s.frazier@gmail.com>
2016-01-16 14:06:38 -0600

    Refactored hashTable.js to correctly pass all tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,26 +3,31 @@ var HashTable = function(){
   this._storage = LimitedArray(this._limit);
 };
 
+
+
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  
-  // this._storage.set(i, v);
-  
-  
+  var hold = {};
+  hold[k] = v;
   
   //if something is already at index i
   if(this._storage.get(i)) {
-    
-    //create a new array at i
-    var tuple = [];
-    //insert existing value and the new value
-    tuple.push(this._storage.get(i));
-    tuple.push(v);
-    this._storage.set(i, tuple);
+    if(this._storage.get(i).hasOwnProperty(k)){
+      this._storage.get(i)[k] = v;
+      // this._storage.set(i)[k] = v;
+    }else{
+      //create a new array at i
+      var tuple = [];
+      //insert existing value and the new value
+      tuple.push(this._storage.get(i));
+      tuple.push(hold);
+      this._storage.set(i, tuple);
+    }
   }else{
-  //if i is unoccupied
-    this._storage.set(i, v);
+    //if i is unoccupied
+    this._storage.set(i, hold);
   }
+  
 };
 
 HashTable.prototype.retrieve = function(k){
@@ -30,34 +35,26 @@ HashTable.prototype.retrieve = function(k){
   
   if(Array.isArray(this._storage.get(i))) {
     for(var j = 0; j < this._storage.get(i).length; j++) {
-      if(this._storage.get(i)[j] === k) { //works well if the key and value are the same thing
-        return this._storage.get(i)[j];
+      if(this._storage.get(i)[j].hasOwnProperty(k)) { 
+        return this._storage.get(i)[j][k];
       }
     }
-  } else {
-    return this._storage.get(i);  
-  }
+  } 
+  return this._storage.get(i)[k];
 };
 
 HashTable.prototype.remove = function(k){
   var indexOfKey = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(indexOfKey, null);
+  var currNode = this._storage.get(indexOfKey);
+  currNode[k] = null;
+
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
- 
- 
- 
- 
- newHash =  {
-  limit : 8
-  storage : {
-    get:
-    set:
-    each:
-  }
- }
+  insert = 0(1)
+  retrieve = 0(n)
+  remove = 0(1)
  */
